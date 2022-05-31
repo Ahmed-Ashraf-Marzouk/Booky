0c0876114cda6976fcd5afb0950c2fb02262ba48
Ahmed Ali <Ahmed.A.Qenawy.gmail.com>
2022-05-31 15:29:00 +0200

    adding slider component and editing layout in all-members

--- js/slider.js
+++ js/slider.js
@@ -1,45 +1,33 @@
 // counter to be used to move between images
-let counter = 0;
+let counter = 1;
 
-// all images to be displayed in the slideshow
-let myImages = [
-  "images/latest0.jpg",
-  "images/latest1.jpg",
-  "images/latest2.jpg",
-  "images/latest3.jpg",
-  "images/latest4.jpg",
-  "images/latest5.jpg",
-  "images/latest6.jpg",
-  "images/latest7.jpg",
-  "images/latest8.jpg",
-];
+const time = 3000; // time for transition --> in miliseconds
 
-const time = 2000; // time for transition --> in miliseconds
-
-let myButtons = document.querySelectorAll("button");
+let rightButton = document.querySelector("[data-button-right]");
+let leftButton = document.querySelector("[data-button-left]");
+let topMember = document.querySelector("[data-top]");
+let dots = document.querySelectorAll("[data-dot]");
 
 // previous button
-myButtons[0].addEventListener("click", function () {
-  counter > 0 ? counter-- : (counter = myImages.length - 1);
-  console.log(counter);
-  document.querySelector(".my-images").src = myImages[counter];
+leftButton.addEventListener("click", function () {
+  counter > 1 ? counter-- : (counter = 5);
+  topMember.src = `media/images/${counter}.jpg`;
 });
 
 // next button
-myButtons[1].addEventListener("click", function () {
-  counter < myImages.length - 1 ? counter++ : (counter = 0);
-
-  console.log(counter);
-  document.querySelector(".my-images").src = myImages[counter];
+rightButton.addEventListener("click", function () {
+  counter <= 5 ? counter++ : (counter = 1);
+  topMember.src = `media/images/${counter}.jpg`;
 });
 
 // auto sliding
+
 function slideShow() {
-  document.querySelector(".my-images").src = myImages[counter];
+  topMember.src = `media/images/${counter}.jpg`;
   console.log(counter);
-  counter < myImages.length - 1 ? counter++ : (counter = 0);
+  dots[counter - 1].classList.toggle("dot--fill");
+  counter < 5 ? counter++ : (counter = 1);
   setTimeout("slideShow()", time);
 }
 
-// Run function when page loads
 window.onload = slideShow;
